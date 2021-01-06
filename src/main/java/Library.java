4e47647c4c004b49b8215b8905d65f191405a2a5
Katie Sloan <katie.a.sloan@gmail.com>
2021-01-06 22:14:51 +0000

    Test for library starting empty passed

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> collection;
+
+    public Library(){
+        this.collection = new ArrayList<Book>();
+    }
+
+    public int bookCount() {
+        return this.collection.size();
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,4 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    Library library;
+    Book book1;
+    Book book2;
+    Book book3;
+
+    @Before public void setUp() {
+        library = new Library();
+        book1 = new Book("Becoming", "Michelle Obama", "autobiography");
+        book2 = new Book("Troy", "Stephen Fry", "historical fiction");
+        book3 = new Book("Quick Cooking", "Mary Berry", "Cookery");
+    }
+
+    @Test
+    public void libraryStartsEmpty(){
+        assertEquals(0, library.bookCount());
+
+    }
 
 
 
