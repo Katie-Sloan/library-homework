486edf2ec523d5b51ad9a2f5c6c56dfd1fb8ab97
Katie Sloan <katie.a.sloan@gmail.com>
2021-01-06 23:08:04 +0000

    Borrower starts with no books

---
+++ src/test/java/BorrowerTest.java
@@ -0,0 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BorrowerTest {
+
+    Borrower borrower;
+    Book book1;
+    Library library;
+
+    @Before
+    public void setUp() {
+        borrower = new Borrower("Katie");
+        book1 = new Book ("Becoming", "Michelle Obama", "autobiography");
+        library = new Library();
+    }
+
+    @Test
+    public void startsWithNoBooks() {
+        assertEquals(0, borrower.bookCount());
+    }
+
+//    @Test
+//    public void canBorrowBook() {
+//        library.add(book1);
+//        borrower.borrowBook();
+//        assertEquals(0, library.bookCount());
+//        assertEquals(1, borrower.bookCount());
+//
+//    }
+
+}
